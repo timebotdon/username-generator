98abdc59066564c1ee29c5b703b73c1d0ef0f691
timebotdon <choyyld@posteo.net>
2021-04-04 16:53:58 +0800

    add yargs, features, update readme, names

--- README.md
+++ README.md
@@ -1,8 +1,53 @@
-Generate random usernames based on a list
+Generate random usernames based on a list!
 
+# Installation
+* Install `npm install`
+
+# Usage
+## Arguments
+### --import
+Mandatory. Imports a name list, use `-i` for shorthand.
+### --format
+Format the outputted username:
+  * name - grab a name from the imported name list
+  * spec - insert a special character. Currently defined: `.-+_!`
+  * num - use a random number between 0-9.
+  * lett - use a random letter between a-z.
+
+If `--format` is not specified, the default format output will follow `name name num`
+
+### --maxnames
+Not mandatory. Specifies the number of usernames to generate. Use
+`-m` for shorthand.
+
+## Examples
+Generate a formatted username
+```
+node app.js --import names.txt --format name name num spec
+
+nursegamora8!
+```
+
+Arbitrary letters/numbers can also be used ('-' can't be used however.). E.g:
+```
+node app.js --import names.txt --format I love name lett
+
+Ilovejokerb
+```
+
+Generate 5 usernames that conforms to a `spec name + name spec` format:
+
+```
+node app.js --import names.txt --maxnames 5 --format spec name + name spec 
+
+-maverick+kingpin!
+-captain+hulk!
+_black+deadpool+
++heart+tarantula.
+_schism+caller+
+```
+
+# Notes 
 Predefined list was compiled from:
-* [Super hero names](https://hobbylark.com/comic-books/superhero-names)
-* [Villian names](https://hobbylark.com/writing/cool-villain-names)
-
-* Install randexp: `npm i randexp`
-* Run: `node app.js`
+1. [Super hero names](https://hobbylark.com/comic-books/superhero-names)
+2. [Villian names](https://hobbylark.com/writing/cool-villain-names)
--- app.js
+++ app.js
@@ -1,22 +1,93 @@
 const randExp = require('randexp');
-const fs = require('fs');
+const { readFileSync } = require('fs');
+const yargs = require('yargs');
+const { exit } = require('process');
 
-const filePath = './names.txt'
+const argv = yargs
+    .option('import', {
+        alias: 'i',
+        desc: 'Import a name list file',
+        type: 'string',
+        demandOption: true
+    })
+    .option('format', {
+        alias: 'f',
+        desc: 'Format the output of the username',
+        type: 'array',
+        demandOption: false
+    })
+    .option('maxnames', {
+        alias: 'm',
+        desc: 'max number of usernames to generate',
+        type: 'number',
+        demandOption: false
+    })
+    .argv;
 
-const data = fs.readFileSync(filePath, {encoding: 'utf8', flag:'r'})
+const data = readFileSync(argv.import, { encoding: 'utf8', flag: 'r' })
 const dic = data.split('\r\n');
 
 // grab random username
-function n(){
-  const rand = Math.floor(Math.random() * dic.length)
-  return rand
+function randomName() {
+    const name = Math.floor(Math.random() * dic.length);
+    return dic[name];
 }
 
-const sep = new randExp(/(\.|-| |_)/).gen();
-const word = new randExp(/[a-z]{0,2}/).gen();
-const num = new randExp(/[0-9]{0,2}/).gen();
-const aNum = new randExp(/\w{0,2}/).gen();
+function randomSpecialChar() {
+    const chars = [".", "-", "_", "+", "!"];
+    return chars[Math.floor(Math.random() * chars.length)];
+}
 
-const format = sep + dic[n()] + dic[n()] + aNum + sep;
 
-console.log(format)
+function randomLetter() {
+    const letter = new randExp(/[a-z]/).gen();
+    return letter;
+}
+
+function randomNumber() {
+    return (Math.floor(Math.random() * 9));
+}
+
+function generate() {
+
+    var newName = [];
+    if (argv.format != undefined) {
+        for (e = 0; e < argv.format.length; e += 1) {
+            switch (argv.format[e]) {
+                case "name":
+                    newName.push(randomName())
+                    break
+                case "lett":
+                    newName.push(randomLetter())
+                    break
+                case "spec":
+                    newName.push(randomSpecialChar())
+                    break
+                case "num":
+                    newName.push(randomNumber())
+                    break
+                default:
+                    newName.push(argv.format[e])
+            }
+        }
+    } else {
+        newName.push(randomName());
+        newName.push(randomName());
+        newName.push(randomSpecialChar());
+    }
+
+    const final = newName.join("");
+    return final;
+}
+
+if (argv.maxnames == undefined) {
+    for (i = 0; i < 1; i += 1) {
+        console.log(generate());
+    }
+} else {
+    for (i = 0; i < argv.maxnames; i += 1) {
+        console.log(generate());
+    }
+}
+
+exit(0)
